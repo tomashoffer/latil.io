00ae17cf023d69e29e185862dadb5ead8e40bf04
tomasmhoffer <tomashoffer96@gmail.com>
2025-12-30 21:27:20 -0300

    add calendry ale

--- lib/config.ts
+++ lib/config.ts
@@ -6,5 +6,5 @@
 // You can also set it via environment variable: NEXT_PUBLIC_CALENDLY_URL
 // Example: "https://calendly.com/your-username/15min"
 // To find your Calendly URL: Go to Calendly > Event Types > Copy the link
-export const CALENDLY_URL = "https://calendly.com/latil-io/meet-latil-io"
+export const CALENDLY_URL = "https://calendly.com/alelatil/30min"
 
